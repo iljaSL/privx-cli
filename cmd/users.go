ad9531373ed7527895597b33364745b2af338168
Ilja Smelich <ilja.smelich@ssh.com>
2021-08-11 10:39:37 +0300

    feat: add new role store related cmds to users

--- cmd/users.go
+++ cmd/users.go
@@ -8,6 +8,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -16,46 +17,56 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
+type userOptions struct {
 	userID         string
-	password       string
-	query          string
-	offset         int
-	limit          int
-	userQuery      []string
+	enable         bool
+	disable        bool
+	reset          bool
+	sources        []string
+	keywords       []string
 	userRoleGrant  []string
 	userRoleRevoke []string
-)
+}
 
 func init() {
-	rootCmd.AddCommand(userListCmd)
-	userListCmd.Flags().StringArrayVarP(&userQuery, "query", "q", []string{}, "query PrivX users with keyword")
-
-	userListCmd.AddCommand(userShowCmd)
-
-	userListCmd.AddCommand(usersRolesCmd)
-	usersRolesCmd.Flags().StringVar(&userID, "uid", "", "user unique id")
-	usersRolesCmd.Flags().StringArrayVar(&userRoleGrant, "grant", []string{}, "grant role to user, requires role unique id.")
-	usersRolesCmd.Flags().StringArrayVar(&userRoleRevoke, "revoke", []string{}, "revoke role from user, requires role unique id.")
-	usersRolesCmd.MarkFlagRequired("uid")
+	rootCmd.AddCommand(userListCmd())
 }
 
 //
 //
-var userListCmd = &cobra.Command{
-	Use:   "users",
-	Short: "PrivX users",
-	Long:  `List and manage PrivX users`,
-	Example: `
-privx-cli users [access flags]
-	`,
-	SilenceUsage: true,
-	RunE:         userList,
+func userListCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "users",
+		Short: "List and manage users",
+		Long:  `List and manage users`,
+		Example: `
+	privx-cli users [access flags] --keywords <KEYWORD>,<KEYWORD>
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userList(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringArrayVarP(&options.keywords, "keywords", "", []string{}, "search keywords")
+
+	cmd.AddCommand(userShowCmd())
+	cmd.AddCommand(userSettingShowCmd())
+	cmd.AddCommand(userSettingsUpdateCmd())
+	cmd.AddCommand(usersRolesCmd())
+	cmd.AddCommand(userMFACmd())
+	cmd.AddCommand(externalUserSearchCmd())
+
+	return cmd
 }
 
-func userList(cmd *cobra.Command, args []string) error {
+func userList(options userOptions) error {
 	api := rolestore.New(curl())
-	users, err := api.SearchUsers(strings.Join(userQuery, ","), "")
+
+	users, err := api.SearchUsers(strings.Join(options.keywords, ","), "")
 	if err != nil {
 		return err
 	}
@@ -65,24 +76,35 @@ func userList(cmd *cobra.Command, args []string) error {
 
 //
 //
-var userShowCmd = &cobra.Command{
-	Use:   "show",
-	Short: "Description about PrivX user",
-	Long:  `Description about PrivX user`,
-	Example: `
-privx-cli users show [access flags] UID ...
-	`,
-	Args:         cobra.MinimumNArgs(1),
-	SilenceUsage: true,
-	RunE:         userShow,
+func userShowCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "show",
+		Short: "Description about PrivX user",
+		Long:  `Description about PrivX user. User ID's are separated by commas when using multiple values, see example`,
+		Example: `
+	privx-cli users show [access flags] --id <USER-ID>,<USER-ID>
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userShow(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&options.userID, "id", "", "user ID")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
 }
 
-func userShow(cmd *cobra.Command, args []string) error {
+func userShow(options userOptions) error {
 	api := rolestore.New(curl())
 	users := []rolestore.User{}
 
-	for _, uid := range args {
-		user, err := api.User(uid)
+	for _, id := range strings.Split(options.userID, ",") {
+		user, err := api.User(id)
 		if err != nil {
 			return err
 		}
@@ -94,41 +116,247 @@ func userShow(cmd *cobra.Command, args []string) error {
 
 //
 //
-var usersRolesCmd = &cobra.Command{
-	Use:   "roles",
-	Short: "user roles",
-	Long:  `list and manage user roles`,
-	Example: `
-privx-cli users roles [access flags] --uid UID
-privx-cli users roles [access flags] --uid UID --grant role-uid
-privx-cli users roles [access flags] --uid UID --revoke role-uid
-	`,
-	SilenceUsage: true,
-	RunE:         userRoles,
+func userSettingShowCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "settings",
+		Short: "Show specific user settings",
+		Long:  `Show specific user settings.`,
+		Example: `
+	privx-cli users settings [access flags] --id <USER-ID>
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userSettingShow(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&options.userID, "id", "", "user ID")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
 }
 
-func userRoles(cmd *cobra.Command, args []string) error {
+func userSettingShow(options userOptions) error {
 	api := rolestore.New(curl())
 
-	for _, role := range userRoleGrant {
-		err := api.GrantUserRole(userID, role)
+	settings, err := api.UserSettings(options.userID)
+	if err != nil {
+		return err
+	}
+
+	return stdout(settings)
+}
+
+//
+//
+func userSettingsUpdateCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "update-settings",
+		Short: "Update specific user's settings",
+		Long:  `Update specific user's settings`,
+		Example: `
+	privx-cli users update-settings [access flags] JSON-FILE --id <USER-ID>
+		`,
+		Args:         cobra.ExactArgs(1),
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userSettingsUpdate(options, args)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&options.userID, "id", "", "user ID")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
+}
+
+func userSettingsUpdate(options userOptions, args []string) error {
+	var updateSettings *json.RawMessage
+	api := rolestore.New(curl())
+
+	err := decodeJSON(args[0], &updateSettings)
+	if err != nil {
+		return err
+	}
+
+	err = api.UpdateUserSettings(updateSettings, options.userID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//
+//
+func usersRolesCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "roles",
+		Short: "Show and manage specific user roles",
+		Long:  `Show and manage specific user roles`,
+		Example: `
+	privx-cli users roles [access flags] --id <USER-ID>
+	privx-cli users roles [access flags] --id <USER-ID> --grant <ROLE-ID>
+	privx-cli users roles [access flags] --id <USER-ID> --revoke <ROLE-ID>
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userRoles(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&options.userID, "id", "", "user ID")
+	flags.StringArrayVar(&options.userRoleGrant, "grant", []string{}, "grant role to user, requires role unique id.")
+	flags.StringArrayVar(&options.userRoleRevoke, "revoke", []string{}, "revoke role from user, requires role unique id.")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
+}
+
+func userRoles(options userOptions) error {
+	api := rolestore.New(curl())
+
+	for _, role := range options.userRoleGrant {
+		err := api.GrantUserRole(options.userID, role)
 		if err != nil {
 			return err
 		}
 	}
 
-	for _, role := range userRoleRevoke {
-		err := api.RevokeUserRole(userID, role)
+	for _, role := range options.userRoleRevoke {
+		err := api.RevokeUserRole(options.userID, role)
 		if err != nil {
 			return err
 		}
 	}
 
-	roles, err := api.UserRoles(userID)
+	roles, err := api.UserRoles(options.userID)
 	if err != nil {
 		return err
 	}
 	return stdout(roles)
+}
+
+//
+//
+func userMFACmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "mfa",
+		Short: "Enable, disable or reset multifactor authentication",
+		Long:  `Enable, disable or reset multifactor authentication. User ID's are separated by commas when using multiple values, see example`,
+		Example: `
+	privx-cli users mfa [access flags] --id <USER-ID>,<USER-ID> --enable
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userMFA(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&options.userID, "id", "", "user ID")
+	flags.BoolVarP(&options.enable, "enable", "e", false, "turn on multifactor authentication")
+	flags.BoolVarP(&options.disable, "disable", "d", false, "turn off multifactor authentication")
+	flags.BoolVarP(&options.reset, "reset", "r", false, "reset multifactor authentication")
+	cmd.MarkFlagRequired("id")
+
+	return cmd
+}
+
+func userMFA(options userOptions) error {
+	if options.enable {
+		enableMFA(options)
+	} else if options.disable {
+		disableMFA(options)
+	} else if options.reset {
+		resetMFA(options)
+	} else {
+		fmt.Fprintln(os.Stderr, "Error: you have to specify one of the following flag: --enable, --disable or --reset")
+		os.Exit(1)
+	}
+
+	return nil
+}
+
+func enableMFA(options userOptions) error {
+	api := rolestore.New(curl())
+
+	err := api.EnableMFA(strings.Split(options.userID, ","))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func disableMFA(options userOptions) error {
+	api := rolestore.New(curl())
+
+	err := api.DisableMFA(strings.Split(options.userID, ","))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resetMFA(options userOptions) error {
+	api := rolestore.New(curl())
+
+	err := api.ResetMFA(strings.Split(options.userID, ","))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//
+//
+func externalUserSearchCmd() *cobra.Command {
+	options := userOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "search",
+		Short: "Search external users",
+		Long:  `Search external users`,
+		Example: `
+	privx-cli users search-external-users [access flags] --keywords <KEYWORD>,<KEYWORD>
+	privx-cli users search-external-users [access flags] --keywords <KEYWORD> --sources <SOURCE>,<SOURCE>
+		`,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return externalUserSearch(options)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringArrayVarP(&options.keywords, "keywords", "", []string{}, "search keywords")
+	flags.StringArrayVarP(&options.sources, "sources", "", []string{}, "the source ID where to search the user from")
+
+	return cmd
+}
+
+func externalUserSearch(options userOptions) error {
+	api := rolestore.New(curl())
+	users, err := api.SearchUsersExternal(strings.Join(options.keywords, ","),
+		strings.Join(options.sources, ","))
+	if err != nil {
+		return err
+	}
+
+	return stdout(users)
 }
 
 func decodeJSON(name string, object interface{}) error {
