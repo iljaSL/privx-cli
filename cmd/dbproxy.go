00657932fb5bedbfaef96f3e5a90803cf0bc5906
GunnerUjjwol <gunner04ujjwol@gmail.com>
2023-07-26 16:08:02 +0300

    Cleanup: remove status command and typo fix

--- cmd/dbproxy.go
+++ cmd/dbproxy.go
@@ -18,52 +18,19 @@ func init() {
 func dbProxyCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "db-proxy",
-		Short: "Db-proxy service status and config commands",
-		Long:  `Show status and configuration for db-proxy service`,
+		Short: "Db-proxy service commands",
+		Long:  `Db-proxy service commands`,
 		Example: `
-	privx-cli db-proxy status [access flags]
 	privx-cli db-proxy config [access flags]
 		`,
 		SilenceUsage: true,
 	}
 
-	cmd.AddCommand(dbproxyStatusCmd())
 	cmd.AddCommand(dbproxyConfCmd())
 
 	return cmd
 }
 
-//
-//
-func dbproxyStatusCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "status",
-		Short: "Get db-proxy microservice status",
-		Long:  `Get db-proxy microservice status. The service status could be one of STOPPED, INITIALIZING, AUTHORIZING, ERROR`,
-		Example: `
-	privx-cli db-proxy status [access flags]
-		`,
-		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return dbproxyStatus()
-		},
-	}
-
-	return cmd
-}
-
-func dbproxyStatus() error {
-	api := dbproxy.New(curl())
-	status, err := api.DbProxyStatus()
-	if err != nil {
-		return err
-	}
-
-	return stdout(status)
-}
-
-//
-//
 func dbproxyConfCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "config",
@@ -83,10 +50,10 @@ func dbproxyConfCmd() *cobra.Command {
 
 func dbproxyConf() error {
 	api := dbproxy.New(curl())
-	status, err := api.DbProxyConf()
+	config, err := api.DbProxyConf()
 	if err != nil {
 		return err
 	}
 
-	return stdout(status)
+	return stdout(config)
 }
